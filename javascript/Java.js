62338bdae9f088dd695d09f65686904a55878624
Maverick <Mave3624@gmail.com>
2024-10-05 22:47:13 +0100

    fixed invalid input errors

--- javascript/Java.js
+++ javascript/Java.js
@@ -17,6 +17,7 @@ function divs( ) {
     }
     message.textContent = 'Total Boxes = ' + NumberOfGrid
     message.style.color = 'green'
+    message.style.border = '1px solid green'
 const grids = document.querySelectorAll('.grid')
 
 grids.forEach(grid => {
@@ -28,37 +29,32 @@ grids.forEach(grid => {
 
 divs()
 
+submit.addEventListener('mousedown', () => {
+    if (input.value > 100 || isNaN(input.value)) {
+    submit.disabled = true
+    message.innerHTML = 'Please Enter A Valid Number From 1 - 100'
+    message.style.color = 'red'
+    message.style.border = '1px solid red'
+    input.value = ''
+}}
+)
+
 submit.addEventListener('click', function hi() {
     UserInput = Number(input.value)
-    console.log(UserInput)
     if (UserInput === 0) UserInput = 10
     NumberOfGrid = UserInput * UserInput
     divs()
     input.value = ''
 })
 
-submit.addEventListener('mousedown', () => {
-    if (input.value > 100) {
-    submit.disabled = true
-    message.textContent = 'Enter a number From 1 - 100'
-    message.style.color = 'red'
-    input.value = ''
-}
-})
-
 input.addEventListener('click', () => {
     submit.disabled = false
     message.textContent = 'Total Boxes = ' + NumberOfGrid
     message.style.color = 'green'
+    message.style.border = '1px solid green'
 })
 
 function randomcolor() {
     let color = Math.floor(Math.random()*256)
-    return color
-}
-// not in use yet
-function opecity() {
-    let color = Math.random().toFixed(3)
-    if (color < 0.8) color = 0.8
     return color
 }
